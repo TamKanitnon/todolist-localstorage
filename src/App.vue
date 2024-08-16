e663a37d406e4ea87a4f7c17a4bbae5504692f7f
TamKanitnon <TamKanitnon@hotmail.com>
2024-08-16 10:15:18 +0700

    start git

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue';
+
+const todos = ref([]);
+const name = ref('');
+
+const input_content = ref('');
+const input_category = ref(null);
+
+// const todos_asc = computed(() => todos.value.sort((a, b) => {
+//   return b.createdAt - a.createdAt;
+// }));
+function todos_asc() {
+  computed(() => todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  }));
+}
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name');
+});
+</script>
+
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+      9:00 https://www.youtube.com/watch?v=qhjxAP1hFuI
+    </section>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
